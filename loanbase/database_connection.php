acdc92b529c84cc384542abf32591960f4ce62b1
Your Name <notroot@ubuntu.(none)>
2015-05-12 19:46:33 +0000

    updating password for database .. tidying

--- loanbase/database_connection.php
+++ loanbase/database_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "rootbase";
+$password = "rootpass";
 $dbname = "borrowbase";
 
 // Create connection
--- loanbase/deploy.bash
+++ loanbase/deploy.bash
@@ -1,8 +1,3 @@
 #!/bin/bash
-cp new_loans.php /var/www
-cp old_loans.php /var/www
-cp index.php /var/www
-cp users.php /var/www
-cp items.php /var/www
-cp footer.php /var/www
+cp *.php /var/www
 
