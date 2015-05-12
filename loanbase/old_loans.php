6c941d542fd4ca5fcc48ea5f495c2172f36a3979
kennj87 <mail@kennj.com>
2015-05-12 14:48:53 +0200

    Update old_loans.php
    Format / Structure

--- loanbase/old_loans.php
+++ loanbase/old_loans.php
@@ -1,15 +1,6 @@
+<?php include_once 'header.php';?>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "rootpass";
-$dbname = "borrowbase";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include_once 'database_connection.php';
 
 $sql = "select * from borrowers join items join transactions where borrowers.borower_id = transactions.borrower_id and items.item_id = transactions.item_id  group by  Borrower_Name ;";
 $result = mysqli_query($conn, $sql);
@@ -17,12 +8,11 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "The User: " . $row["Borrower_Name"]. " Has borrowed: " . $row["Item_Name"]. "<br>". "\n" ;
+        echo "The User: " . $row["Borrower_Name"]. " Has borrowed: " . $row["Item_Name"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 
 mysqli_close($conn);
-?> 
-<?php include 'footer.php';?>
+include_once 'footer.php';
